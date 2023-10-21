498b09a3660a2abc3f0ff23f572cb6623dab530c
sziligunz <sziligunz@gmail.com>
2023-10-21 13:37:38 +0200

    mm: fixed mm button spamming

--- app/src/app/pages/home/home.component.ts
+++ app/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { Router } from '@angular/router';
+import { first } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { SnackbarService } from 'src/app/services/snackbar.service';
 import { SocketService } from 'src/app/services/socket.service';
@@ -25,11 +26,11 @@ export class HomeComponent {
     private snackbarService: SnackbarService,
     private router: Router
     ) {
-      socketService.roomHash.subscribe(newRoomHash => {
+      socketService.roomHash.pipe(first()).subscribe(newRoomHash => {
         this.mmButton.disabled = true
         this.spinSpinner = false
         this.roomHash = newRoomHash
-        this.snackbarService.createCheck("MATCH FOUND")
+        this.snackbarService.createCheck("Match Found")
           .afterDismissed().subscribe(event => {
             // TODO: navigate to game screen
             // this.router.navigateByUrl("game")
@@ -39,6 +40,7 @@ export class HomeComponent {
 
   async startMatchmaking() {
     this.spinSpinner = true
+    this.snackbarService.createCheck("Looking for match")
     const user = await this.userCrudService.getLoggedInUser(await this.authService.getCurrentUser())
     this.socketService.socket.emit("join-mm", {userId: user?.email})
   }

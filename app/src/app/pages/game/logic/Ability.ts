8f7a0f852cc08e780d2a5e0c34219424bbbc9e98
sziligunz <sziligunz@gmail.com>
2024-02-20 20:19:34 +0100

    gamelogic-2 corrected ability poisition of SalvoFiring

--- app/src/app/pages/game/logic/Ability.ts
+++ app/src/app/pages/game/logic/Ability.ts
@@ -237,11 +237,11 @@ export class SalvoFiringAbility extends Ability {
             {x: -1, y: -1},
             {x: -2, y: -1},
             {x: -3, y: -1},
-            {x: -3, y: -1},
             {x: -1, y: 1},
             {x: -2, y: 1},
             {x: -3, y: 1},
-            {x: -3, y: 1}
+            {x: -1, y: 0},
+            {x: -2, y: 0},
         ]
     }
 }
